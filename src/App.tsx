b5fcf93d4c4b4e8a17ee9b5430ec2f35a48bdb34
Aleksey Verin <verevaa@yandex.ru>
2023-06-17 11:13:14 +0300

    feat: added dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import '@/styles/App.scss';
+// import '@/styles/App.scss';
 import AppRouter from './components/AppRouter';
 import LayoutContainer from './components/layouts/LayoutContainer';
 import { store } from './store/store';
